cd7c26777440ed00ee6e98445b9b46794ff9af34
hochlerer <hb@sem-wolf.org>
2020-07-26 23:59:56 +0300

    Adding the row count feature in the table

--- db.py
+++ db.py
@@ -26,6 +26,35 @@ class DBTable:
     fields: List[DBField]
     key_field_name: str
 
+    def count(self) -> int:
+        file_name = open(f'{self.name}.db')
+        try:
+            count = len(file_name[self.name].keys())
+        finally:
+            file_name.close()
+        return count
+
+    def insert_record(self, values: Dict[str, Any]) -> None:
+        raise NotImplementedError
+
+    def delete_record(self, key: Any) -> None:
+        raise NotImplementedError
+
+    def delete_records(self, criteria: List[SelectionCriteria]) -> None:
+        raise NotImplementedError
+
+    def get_record(self, key: Any) -> Dict[str, Any]:
+        raise NotImplementedError
+
+    def update_record(self, key: Any, values: Dict[str, Any]) -> None:
+        raise NotImplementedError
+
+    def query_table(self, criteria: List[SelectionCriteria]) \
+            -> List[Dict[str, Any]]:
+        raise NotImplementedError
+
+    def create_index(self, field_to_index: str) -> None:
+        raise NotImplementedError
 
 
 @dataclass_json
